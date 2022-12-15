ece997d9f256aa3630aac6fb4ae11f0657f061e0
BLacey99 <81555350+BLacey99@users.noreply.github.com>
2022-12-15 16:23:32 -0600

    Began base work for e-commerce demo site. Nav bar, experimental listing component for items, footer, some responsive styling.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ListBuilder from './components/listBuilder';
+import NavBar from './components/nav';
+import Footer from './components/footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     <NavBar/>
+     <ListBuilder/>
+     <Footer/>
+     
+
     </div>
   );
 }
