4624c85e2619ee3ed2c2719888f762122e0d70d1
BLacey99 <81555350+BLacey99@users.noreply.github.com>
2022-12-16 16:06:34 -0600

    Added color value detection on card generation.

--- src/components/listCont/listContainer.js
+++ src/components/listCont/listContainer.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import listContStyles from "./listContainer.css";
-
+import { useState } from "react";
 
 export default function Lists (props){
    
-   
+   /*  const [colLor, setColor] = useState(props.data.value);
+    
+    function colorize(){
+            setColor(props.data.value);
+        console.log(colLor);
+      
+    }
+    */
+        const colorApplication = {
+        color: props.data.value,
+    };
+
+
     return(
-        <div id="product-container" key={props.data.id}>
+        <div id="product-container" /* onClick={colorize} */ key={props.data.id} style={colorApplication}>
             <small>{props.data.id}</small><h3>{props.data.value}</h3>
-            <p className='color'>This color is:{props.data.color}</p>
+            <p className='color' >This color is: {props.data.color}</p>
+          
         </div>
        
     )
