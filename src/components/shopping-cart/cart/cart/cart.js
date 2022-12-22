bdf65438a73c4cd2f211a7431a6892f203046692
BLacey99 <81555350+BLacey99@users.noreply.github.com>
2022-12-22 13:53:43 -0600

    Added cart icon and functionality. TODO: Add cart page

--- src/components/shopping-cart/cart/cart/cart.js
+++ src/components/shopping-cart/cart/cart/cart.js
@@ -1,14 +1,19 @@
-import React from "react"
-import './cart.css'
+import React from "react";
+import "./cart.css";
 import { useState } from "react";
+import CartIcon from "./cartIcon.png";
 
-export default function Cart(props){
-
-    return(
-        <div className="cartClass">
-            <p>Total: ${props.total.toFixed(2)}</p>
-            <p>Cart: {props.cartSize}</p>
+export default function Cart(props) {
+  return (
+    <div className="cartClass">
+      <div id="cartContainer">
+      <a href="http://localhost:3000/"><img id="cartIcon" src={CartIcon}></img></a>
+        <div id="cartSize">
+          <p>{props.cartSize}</p>
         </div>
-    )
+      </div>
 
+      <p>Total: ${props.total.toFixed(2)}</p>
+    </div>
+  );
 }
